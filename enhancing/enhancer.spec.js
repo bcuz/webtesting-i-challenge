3d19a003d7ea00f645c31cb605cccfed861c2794
adam <bcuz@users.noreply.github.com>
2019-06-19 21:22:46 -0400

    make cleaner

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -13,17 +13,21 @@ let shield = {
 
 describe('enhancing system', () => {
 
-  it('should restore durability', () => {
-    // arrange. mock data
+  describe('repair function', () => {
+    it('should restore durability', () => {
+      // arrange. mock data
 
-    // act
-    let result = enhancer.repair(sword)
+      // act
+      let result = enhancer.repair(sword)
 
-    // assert 
-     expect(result.durability).toBe(100);
-   })
+      // assert 
+      expect(result.durability).toBe(100);
+    })
+  })
   
-   it('should successfully enhance', () => {
+  describe('success function', () => {
+
+   it('should increment enhancement if its not 20', () => {
     // act
     let result = enhancer.succeed(sword)
     let result1 = enhancer.succeed(shield)
@@ -32,22 +36,23 @@ describe('enhancing system', () => {
      expect(result.enhancement).toBe(15);
      expect(result1.enhancement).toBe(20);
    })
+  })
   
   describe('fail function', () => {
    
    it('should decrease durability', () => {
-    // act
     let result = enhancer.fail(sword)
     let result1 = enhancer.fail(shield)
 
-    // assert 
      expect(result.durability).toBe(20);
      expect(result1.durability).toBe(15);
     })
     
-    it('should decrease enhancement', () => {
+    it('should decrease enhancement when enhancement greater than 16', () => {
+      let result = enhancer.fail(sword)
       let result1 = enhancer.fail(shield)
       
+      expect(result.enhancement).toBe(14);
       expect(result1.enhancement).toBe(19);
 
    })
